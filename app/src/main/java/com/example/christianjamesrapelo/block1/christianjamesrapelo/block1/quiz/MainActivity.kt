6b4c2407c03cae54c39a5fd40dc8f5814621a37d
JamesRapelo <jrapelo25@gmail.com>
2023-12-07 13:18:30 +0800

    First Commit

---
+++ app/src/main/java/com/example/christianjamesrapelo/block1/christianjamesrapelo/block1/quiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.christianjamesrapelo.block1.christianjamesrapelo.block1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/example/christianjamesrapelo/block1/christianjamesrapelo/block1/quiz/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.christianjamesrapelo.block1.christianjamesrapelo.block1.quiz
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
